ccce95e89ef59866b29809c02144b8c8eef898cd
Jamal Naser Ayubi <50467548+jamalayubi@users.noreply.github.com>
2019-07-30 15:04:49 +0200

    Updated

--- Week2/homework/maartjes-work.js
+++ Week2/homework/maartjes-work.js
@@ -3,63 +3,59 @@
 const monday = [
   {
     name: 'Write a summary HTML/CSS',
-    duration: 180,
+    duration: 180
   },
   {
     name: 'Some web development',
-    duration: 120,
+    duration: 120
   },
   {
     name: 'Fix homework for class10',
-    duration: 20,
+    duration: 20
   },
   {
     name: 'Talk to a lot of people',
-    duration: 200,
-  },
+    duration: 200
+  }
 ];
 
 const tuesday = [
   {
     name: 'Keep writing summary',
-    duration: 240,
+    duration: 240
   },
   {
     name: 'Some more web development',
-    duration: 180,
+    duration: 180
   },
   {
     name: 'Staring out the window',
-    duration: 10,
+    duration: 10
   },
   {
     name: 'Talk to a lot of people',
-    duration: 200,
+    duration: 200
   },
   {
     name: 'Look at application assignments new students',
-    duration: 40,
-  },
+    duration: 40
+  }
 ];
 
-const maartjesTasks = monday.concat(tuesday);
-const maartjesHourlyRate = 20;
+const tasks = monday.concat(tuesday);
+// Add your code here
 
-function computeEarnings(tasks, hourlyRate) {
-  // Replace this comment and the next line with your code
-  console.log(tasks, hourlyRate);
-}
+const workingHours = tasks.map(min => min.duration/60);
 
-// eslint-disable-next-line no-unused-vars
-const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+const weightedHours = workingHours.filter(min => { if(min >= 2) return min;});
 
-// add code to convert `earnings` to a string rounded to two decimals (euro cents)
+const perHourIncome = weightedHours.map( x => x * 120);
 
-console.log(`Maartje has earned €${'replace this string with the earnings rounded to euro cents'}`);
+const totalIncome = perHourIncome.reduce((acc, cur)=> { return acc + cur});
 
-// Do not change or remove anything below this line
-module.exports = {
-  maartjesTasks,
-  maartjesHourlyRate,
-  computeEarnings,
-};
+
+console.log('Working Time: ' + workingHours);
+console.log('Weighted more than two hours:' + weightedHours);
+console.log('Gross hourly income: ' + perHourIncome);
+console.log('Total Income in krornor, 120-SEK per hour: ' + totalIncome);
+console.log('Total income in € ' + ((totalIncome)/10.3).toFixed(2));
